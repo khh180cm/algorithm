b50f870a51785f7d5b23ec34b212b8a47e45f3fa
Hyuckhoon Ko <khh180cm@gmail.com>
2021-02-17 22:38:07 +0900

    [ADD] 최빈값 구하기 방법 2개 추가

--- week_1/frequent_value.py
+++ week_1/frequent_value.py
@@ -32,7 +32,7 @@ def find_max_occurred_alphabet_2(string):
     string = sentence.replace(" ", "")
     alphabet_occurrence = [0] * 26
     std_asci = ord("a")
-    # 알파벳 빈도수 카운트
+    # 알파벳 빈도 카운트
     for each in string:
         if each.isalpha():
             asci_num = ord(each)
@@ -49,4 +49,54 @@ def find_max_occurred_alphabet_2(string):
 
 
 result = find_max_occurred_alphabet_2(sentence)
+print(result)
+
+
+# 방법 3
+def find_max_occurred_alphabet_3(string):
+    string = string.replace(" ", "")
+    alphabet = [chr(i) for i in range(97, 123)]
+    alphabet_occurrence = [0] * 26
+
+    for each in alphabet:
+        # 이중 for문으로 빈도 카운트
+        for compare_each in string:
+            if each == compare_each:
+                asci_num = ord(each)
+                idx = asci_num - ord("a")
+                alphabet_occurrence[idx] += 1
+
+    # 최댓값 찾기
+    max_num = float("-inf")
+    max_idx = 0
+    for idx, cnt in enumerate(alphabet_occurrence):
+        if cnt > max_num:
+            max_num = cnt
+            max_idx = idx
+    return alphabet[max_idx]
+
+
+result = find_max_occurred_alphabet_3(sentence)
+print(result)
+
+
+# 방법 4
+def find_max_occurred_alphabet_4(string):
+    string = string.replace(" ", "")
+    alphabet = [chr(i) for i in range(97, 123)]
+
+    max_occurrence = 0
+    max_alphabet = alphabet[0]
+    for each in alphabet:
+        occurrence = 0
+        for compare_each in string:
+            if each == compare_each:
+                occurrence += 1
+        if occurrence > max_occurrence:
+            max_occurrence = occurrence
+            max_alphabet = each
+    return max_alphabet
+
+
+result = find_max_occurred_alphabet_4(sentence)
 print(result)
