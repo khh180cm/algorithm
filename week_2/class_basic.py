20c8dce11c446fcd324676775fa98464c6bb9d5c
Hyuckhoon Ko <khh180cm@gmail.com>
2021-02-22 07:51:34 +0900

    [ADD] 링크드 리스트 구현 전 기본적인 클래스 구현

---
+++ week_2/class_basic.py
@@ -0,0 +1,25 @@
+class Person:
+    def __init__(self, param_name):
+        """
+        Constructor
+        """
+
+        print("I'm created!!!!", self)
+        self.name = param_name
+
+    def talk(self):
+        """
+        talk 메소드
+        """
+        print(f"안녕하세요, 제 이름은 {self.name}입니다.")
+
+
+person_1 = Person("유재석")
+print(person_1.name)
+print(person_1)
+person_1.talk()
+
+person_2 = Person("박명수")
+print(person_2.name)
+print(person_2)
+person_2.talk()
