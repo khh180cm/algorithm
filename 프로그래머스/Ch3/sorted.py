05a30221b13808496c20085c33d5a18d526cadae
khh180cm <khh180cm@gmail.com>
2022-07-22 00:02:26 +0900

    [ADD] 리스트 sorted 예제

---
+++ 프로그래머스/Ch3/sorted.py
@@ -0,0 +1,12 @@
+"""
+sorted로 리스트 정렬하기
+"""
+L = [3, 6, 2, -1, 50, 13, 37, 8]
+
+# 1) 오름차순 정렬
+ordered_L = sorted(L)
+print(ordered_L)
+
+# 2) 내림차순 정렬
+ordered_L = sorted(L, reverse=True)
+print(ordered_L)
