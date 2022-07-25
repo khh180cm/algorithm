6af5b0349a2faa7bff7d5c126bb5978df19f7ffa
khh180cm <khh180cm@gmail.com>
2022-07-25 19:18:54 +0900

    [ADD] 리스트 내장함수 index로 선형 탐색

--- 프로그래머스/Ch3/search.py
+++ 프로그래머스/Ch3/search.py
@@ -1,20 +1,38 @@
 """
-선형탐색
-- O(N)
+선형 탐색
+- 시간복잡도: O(N)
 """
 from typing import List
 
 L = [3, 8, 1, 5, 2, 6, 9]
+TARGET = 6
 
 
-def linear_search(target: int, rand_list: List) -> int:
+def linear_search(rand_list: List, target: int) -> int:
     found = -1
-    for i in rand_list:
-        if i == target:
-            found = i
+    for idx, val in enumerate(rand_list):
+        if val == target:
+            found = idx
             break
     return found
 
 
-res = linear_search(6, L)
+res = linear_search(L, TARGET)
+print(res)
+
+
+"""
+선형 탐색을 리스트의 내장함수(index)로 구하기
+- 시간복잡도: O(N)
+"""
+
+
+def linear_search_by_index(rand_list: List, target: int) -> int:
+    try:
+        return rand_list.index(target)
+    except ValueError:
+        return -1
+
+
+res = linear_search(L, TARGET)
 print(res)
